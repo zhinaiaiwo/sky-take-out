41ed191c6fcb0adb6b3b0e85597cd195a4b0bb03
zhinaiaiwo <192845124@qq.com>
2026-06-09 00:43:54 +0800

    exceptionHandler 修改

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -32,8 +34,13 @@ public class GlobalExceptionHandler {
         // Duplicate entry '123456789012345678' for key 'idx_username'
         String message = ex.getMessage();
         if (message.contains("Duplicate entry")) {
-            String[] split = message.split(" ");
-            String username = split[2];
+//              正常写法
+//            String[] split = message.split(" ");
+//            String username = split[2];
+//              一句话写完的
+//            String split = message.split(" ")[2];
+//              使用 stream() 流的
+            String username = Arrays.stream(message.split(" ")).skip(2).limit(1).collect(Collectors.joining(" "));
             String msg = username + MessageConstant.ACCOUNT_EXISTS;     // 已存在
             return Result.error(msg);
         }else{
